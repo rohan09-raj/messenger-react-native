c36f80b147e020d4e924c5fe952ddbc7812be999
deepak-kumar2507 <cooldeepak2507@gmail.com>
2024-02-27 23:00:45 +0530

    first commit

---
+++ screens/FriendsScreen.js
@@ -0,0 +1,54 @@
+import { StyleSheet, Text, View } from "react-native";
+import React, { useEffect, useContext, useState } from "react";
+import axios from "axios";
+import { UserType } from "../UserContext";
+import FriendRequest from "../components/FriendRequest";
+import { API_BASE_URL } from "../api/constants/api";
+
+const FriendsScreen = () => {
+  const { userId, setUserId } = useContext(UserType);
+  const [friendRequests, setFriendRequests] = useState([]);
+  useEffect(() => {
+    fetchFriendRequests();
+  }, []);
+
+  const fetchFriendRequests = async () => {
+    try {
+      const response = await axios.get(
+        `${API_BASE_URL}/friend-request/${userId}`
+      );
+      if (response.status === 200) {
+        const friendRequestsData = response.data.map((friendRequest) => ({
+          _id: friendRequest._id,
+          name: friendRequest.name,
+          email: friendRequest.email,
+          image: friendRequest.image,
+        }));
+
+        setFriendRequests(friendRequestsData);
+      }
+    } catch (err) {
+      console.log("error message", err);
+    }
+  };
+
+  console.log(friendRequests);
+  return (
+    <View style={{ padding: 10, marginHorizontal: 12 }}>
+      {friendRequests.length > 0 && <Text>Your Friend Requests!</Text>}
+
+      {friendRequests.map((item, index) => (
+        <FriendRequest
+          key={index}
+          item={item}
+          friendRequests={friendRequests}
+          setFriendRequests={setFriendRequests}
+        />
+      ))}
+    </View>
+  );
+};
+
+export default FriendsScreen;
+
+const styles = StyleSheet.create({});
